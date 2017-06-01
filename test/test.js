1cbfabaca88eba3ca0f26cad0348f12ef0c9d427
Josep Subils <josep@java.cat>
2017-06-01 15:33:27 +0200

    [release] 0.0.3

--- test/test.js
+++ test/test.js
@@ -1,14 +1,30 @@
-var cfile= require('../index.js')
+var cfile = require('../index.js')
+var chai = require('chai')
 var arr = [
-"file.jpg",
-"file.html",
-"http://asdasjjpgnpm",
-"mpeg.adsa.png.net",
-"h.png.mpge",
-"h.png.mpg",
-"a.middleware"
+  "file.jpg",
+  "file.html",
+  "http://asdasjjpgnpm",
+  "mpeg.adsa.png.net",
+  "h.png.mpge",
+  "h.png.mpg",
+  "h.pngmpg",
+  "a.middleware"
 ]
 
-arr.forEach((e) => {
-	if (cfile(e)) console.log(`${e} is file.`)
+describe("Checking multiple values", () => {
+  it('Returns a boolean', () => {
+    arr.forEach((e) => {
+      chai.assert.isBoolean(cfile(e))
+    })
+  })
+
+  it('Returns a 2 files', () => {
+    var files = arr.filter(cfile)
+    chai.assert.lengthOf(files, 2)
+  })
+
+  it('Returns a 6 no file', () => {
+    var nofiles = arr.filter((e) => !cfile(e))
+    chai.assert.lengthOf(nofiles, 6)
+  })
 })
