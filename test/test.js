39d9d89c05fc33feb7f6131d0667f30a5f44219f
Josep Subils <js@js.gl>
2019-03-04 10:43:31 +0100

    [test] add new  input for extensions

--- test/test.js
+++ test/test.js
@@ -1,33 +1,44 @@
 /*global describe it*/
-var cfile = require('../index.js');
-var chai = require('chai');
-var arr = [
-  "file.jpg",
-  "file.html",
-  "http://asdasjjpgnpm",
-  "mpeg.adsa.png.net",
-  "h.png.mpge",
-  "h.png.mpg",
-  "h.pngmpg",
-  "a.middleware"
-];
+var cfile = require('../index.js')
+var chai = require('chai')
+var files = [
+  'file.jpg',
+  'file.jpeg',
+  'file.html',
+  'http://asdasjjpgnpm',
+  'mpeg.adsa.png.net',
+  'h.png.mpge',
+  'h.png.mpg',
+  'h.pngmpg',
+  'a.middleware'
+]
 
-describe("Checking multiple values", function () {
-  it('Returns a boolean', function  () {
-    arr.forEach(function (e) {
-      chai.assert.isBoolean(cfile(e));
-    });
-  });
-
-  it('Returns a 2 files', function () {
-    var files = arr.filter(cfile);
-    chai.assert.lengthOf(files, 2);
-  });
-
-  it('Returns a 6 no file', function () {
-    var nofiles = arr.filter(function (e) {
-      return !cfile(e);
+describe('Checking multiple values', () => {
+  it('Returns a boolean', () => {
+    files.forEach(file => {
+      chai.assert.isBoolean(cfile(file))
     })
-    chai.assert.lengthOf(nofiles, 6);
-  });
-});
+  })
+
+  it('Returns a 3 files', () => {
+    var files_filtered = files.filter(cfile)
+    chai.assert.lengthOf(files_filtered, 3)
+  })
+
+  it('Returns a 6 no file', () => {
+    var nofiles = files.filter(e => !cfile(e))
+    chai.assert.lengthOf(nofiles, 6)
+  })
+
+  it('Bolean fixed files', () => {
+    const extensions = ['jpg', 'jpeg']
+    chai.assert.isBoolean(cfile('bueno.jpg', extensions))
+    chai.assert.isTrue(cfile('bueno.jpg', extensions))
+  })
+
+  it('Check list files', () => {
+    const extensions = ['jpg', 'jpeg']
+    var nofiles = files.filter(file => cfile(file, extensions))
+    chai.assert.lengthOf(nofiles, 2)
+  })
+})
